b86911b70fc85a4692a550c00441165919a597ca
MdAbdullahAlMahmud <mmunna864@gmail.com>
2022-01-20 23:30:11 +0600

    GridView

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('ListView'),
+          title: Text('GridView'),
+          backgroundColor: Colors.teal,
         ),
         body: MyApp(),
       ),
@@ -16,7 +17,6 @@ void main() {
   );
 
 }
-
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -26,52 +26,100 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  List<int> items =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  List<int> items = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+  //GridView Count
+  /*@override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            child: Text("Android"),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.teal[100],
+            ),
+          ),
+           Container(
+            width: 100,
+            height: 100,
+             alignment: Alignment.center,
+             decoration: BoxDecoration(
+                 shape: BoxShape.circle,
+               color: Colors.teal[200],
+
+             ),
+            child: Text("Flutter"),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+              color: Colors.teal[300],
+
+            ),
+            child: Text("Java"),
+          ),
+
+        ],
+
+
+
+      ),
+    );
+  }*/
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      scrollDirection: Axis.vertical,
-        padding: EdgeInsets.all(5.0),
-        separatorBuilder: (context, index) => const Divider(color: Colors.blueGrey,
-        thickness: 2.0,),
-        itemCount:items.length,itemBuilder: (BuildContext context,int index)
-    {
-      return Container(
-        width: 400.0,
-          child:
-        ListTile(
-            title: Text("${count} Row number  ${items[index]}"),
-             /*leading: Container(
-          width: 40.0,
-          height: 40.0,
-          child: Icon(Icons.flag),
-          decoration: BoxDecoration(
+    return GridView.builder(
+        itemCount: items.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+          /*mainAxisSpacing: 10,
+          crossAxisSpacing: 10*/
+        ),
+        itemBuilder: (BuildContext context, int index){
+
+          return Padding(
+            padding: const EdgeInsets.all(8.0),/*
+            child: Container(
+                width: 100,
+                height: 100,
+                child: Text("Android"),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
             shape: BoxShape.circle,
-            color: Colors.greenAccent
-          ),
-        )*/
-
-            leading: CircleAvatar(
-              backgroundColor: Colors.greenAccent,
-              child: Icon(Icons.flag),
+            color: Colors.teal[100],
+            )
+            ),*/
+            child: Card(
+              elevation: 10,
+              color: Colors.teal[200],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(60)
+              ),
+              child: Container(
+          width: 100,
+          height: 100,
+          child: Text("Android"),
+          alignment: Alignment.center,
+          /*decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.teal[100],
+              ),*/
             ),
-            trailing: FloatingActionButton(
-              onPressed: onButtonClicked,
-              child: Icon(Icons.add),
-            ),
-          ),
-      );
-    }
-    );
-  }
-
-  int count = 0 ;
-  void onButtonClicked(){
-    setState(() {
-      count++;
-      if(count>=10){
-        count=0;
-      }
+          )
+          );
     });
   }
 }
+
+
+
