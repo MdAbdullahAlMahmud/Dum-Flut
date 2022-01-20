ba348654b310a6186237995970848b75a068510b
MdAbdullahAlMahmud <mmunna864@gmail.com>
2022-01-20 20:38:39 +0600

    static list view

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,104 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(home:Scaffold(
-      body: MyApp(),
-      appBar: AppBar(
-        title: Text("Android Activity"),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView'),
+        ),
+        body: MyApp(),
       ),
-    )), // use MaterialApp
+    )
   );
+
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      //crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text("Flutter"),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Android"),
-            Text("Flutter")
+  _MyAppState createState() => _MyAppState();
+}
 
-          ],
-        ),
-        Text("Flutter")
-      ],
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 600.0,
+      width: double.infinity,
+      color: Colors.blue,
+      child: ListView(
+        children: [
+          ListTile(
+            title: Text("Bangladesh"),
+            leading: Container(
+              height: 50.0,
+              width: 50.0,
+              child: Icon(Icons.flag),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.greenAccent
+              ),
+            ),
+            trailing: Icon(Icons.arrow_forward,
+              color: Colors.white,
+            ),
+            subtitle: Text("Growing Country"),
+            textColor: Colors.white,
+          ),
+          ListTile(
+            title: Text("Pakistan"),
+            leading: Container(
+              height: 50.0,
+              width: 50.0,
+              child: Icon(Icons.flag),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.greenAccent
+              ),
+            ),
+            trailing: Icon(Icons.arrow_forward,
+              color: Colors.white,
+            ),
+            subtitle: Text("Growing Country"),
+            textColor: Colors.white,
+          ),
+          ListTile(
+            title: Text("Pakistan"),
+            leading: Container(
+              height: 50.0,
+              width: 50.0,
+              child: Icon(Icons.flag),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.greenAccent
+              ),
+            ),
+            trailing: Icon(Icons.arrow_forward,
+              color: Colors.white,
+            ),
+            subtitle: Text("Growing Country"),
+            textColor: Colors.white,
+          ),
+          ListTile(
+            title: Text("Pakistan"),
+            leading: Container(
+              height: 50.0,
+              width: 50.0,
+              child: Icon(Icons.flag),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.greenAccent
+              ),
+            ),
+            trailing: Icon(Icons.arrow_forward,
+              color: Colors.white,
+            ),
+            subtitle: Text("Growing Country"),
+            textColor: Colors.white,
+          )
+        ],
+      ),
     );
   }
 }
